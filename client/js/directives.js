5e22a58630a2c529c242228bd78c402d81edae54
AquaSolid <darksands>
2016-07-22 14:36:27 +0200

    8: Fear from Laptop Crash

--- client/js/directives.js
+++ client/js/directives.js
@@ -1,23 +1,3 @@
-/*var compareTo = function() {
-    return {
-        require: "ngModel",
-        scope: {
-            otherModelValue: "=compareTo"
-        },
-        link: function(scope, element, attributes, ngModel) {
-             
-            ngModel.$validators.compareTo = function(modelValue) {
-                return modelValue == scope.otherModelValue;
-            };
- 
-            scope.$watch("otherModelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
-};
-module.directive("compareTo", compareTo);*/
-
 app.directive('signup', function() {
     return {
         restrict: "E",
